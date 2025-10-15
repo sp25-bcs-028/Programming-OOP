39ba374fe8c10001361992ec7a4d69bb24c4ee4f
sp25-bcs-028 <sp25-bcs-028@cuilahore.edu.pk>
2025-10-15 13:25:14 +0100

    Lab 6 Housing Society

--- Housing Society/Lab 6/Plot.java
+++ Housing Society/Lab 6/Plot.java
@@ -4,12 +4,6 @@ public class Plot{
 	private Boolean availability;
 	private Shapes shape;
 	private Double area;
-	private double RES_5_MARLA = 4000000; 
-	private double RES_10_MARLA = 7500000; 
-	private double RES_1_KANAL = 14000000;
-	private double COMM_SHOP = 3000000;
-	private double COMM_OFFICE = 5000000;
-	private double PARKING = 200000;
 	private double plotPrice;
 
 	public Plot(String id, PlotTypes plotType, Boolean availability){
@@ -30,4 +24,7 @@ public class Plot{
 	public void cancelBooking(){
 		retrun this.availability = true;
 	}	
+	public String toString(){
+		return String.format("Plot Type: ")
+	}
 }
